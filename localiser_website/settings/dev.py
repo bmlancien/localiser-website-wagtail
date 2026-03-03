1e4411741856832e8e9171ece66c9bf08bc999a1
Bryan Lancien <bryanlancien.ui@gmail.com>
2026-03-03 17:10:43 +0000

    Initial commit

---
+++ localiser_website/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-@8zai2#m=z6#vn-e&+#&^d+=k6o^%=sv-fwjsmyhp7_w9%96&%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
